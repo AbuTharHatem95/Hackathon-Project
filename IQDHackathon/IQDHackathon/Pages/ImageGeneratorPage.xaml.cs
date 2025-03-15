520cbaa97ae200b7c85b94466b454d136146008e
Mortaza.n.m <mortazanm92@gmail.com>
2025-03-16 01:40:23 +0300

    Created MainWindow, and Pages folder

---
+++ IQDHackathon/IQDHackathon/Pages/ImageGeneratorPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using IQDHackathon;
+
+namespace Interface.Pages
+{
+    public partial class ImageGeneratorPage : Page
+    {
+        public ImageGeneratorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMainWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
+        }
+
+        private void GenerateImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic for generating the image based on user input.
+            string description = ImageDescriptionTextBox.Text;
+
+            if (!string.IsNullOrEmpty(description))
+            {
+                //GeneratedImage.Source = new Uri("https://via.placeholder.com/400x300.png?text=" + description);
+            }
+            else
+            {
+                MessageBox.Show("يرجى إدخال وصف للصورة.");
+            }
+        }
+    }
+}
---
+++ IQDHackathon/IQDHackathon/Pages/StoryCharacterPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using IQDHackathon;
+
+namespace Interface.Pages
+{
+    public partial class StoryCharacterPage : Page
+    {
+        public StoryCharacterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMainWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
+        }
+    }
+}
