187c5e8bc6ad925e87c83de3cbc75c2d424c5616
Abo_Sanad <167923142+AbuTharHatem95@users.noreply.github.com>
2025-03-16 05:10:38 +0300

    Update On MainWindow Useing UserControl (#5)

--- IQDHackathon/IQDHackathon/MainWindow.xaml.cs
+++ IQDHackathon/IQDHackathon/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace IQDHackathon
         public MainWindow()
         {
             InitializeComponent();
-           
+
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
@@ -35,88 +35,108 @@ namespace IQDHackathon
             WindowState = WindowState.Minimized;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+
+        //private void OpenPresentationGenerator(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new PresentationGeneratorPage();
+        //}
+
+        //private void OpenReportAssistant(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new ReportAssistantPage();
+        //}
+
+        //private void OpenTestScenarioGenerator(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new TestScenarioGeneratorPage();
+        //}
+
+        //private void OpenUIDesigner(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new UIDesignerPage();
+        //}
+
+        //private void OpenStoryCharacterGenerator(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new StoryCharacterPage();
+        //}
+
+        //private void OpenFinancialAnalyzer(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new FinancialAnalyzerPage();
+        //}
+
+        //private void OpenEducationMaterialGenerator(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new EducationMaterialPage();
+        //}
+
+        //private void OpenMaintenanceAssistant(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new MaintenanceAssistantPage();
+        //}
+
+        //private void OpenImageGenerator(object sender, MouseButtonEventArgs e)
+        //{
+        //    Content = new ImageGeneratorPage();
+        //}
+
+
+
+
+
+
+        private void SmartFinancialAnalysis_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            LanguageControler.SetLanguage(LanguageControler.enLanguage.English);
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Presentations_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            LanguageControler.SetLanguage(LanguageControler.enLanguage.English);
-        }
-
-        private void rdSettings_Checked(object sender, RoutedEventArgs e)
-        {
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
 
         }
 
-        private void OnMouseEnter(object sender, MouseEventArgs e)
+        private void ReportingAssistant_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Border border = sender as Border;
-            if (border != null)
-            {
-                border.RenderTransform = new ScaleTransform(1.05, 1.05);
-            }
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
+
         }
 
-        private void OnMouseLeave(object sender, MouseEventArgs e)
+        private void TestScenarioGenerator_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Border border = sender as Border;
-            if (border != null)
-            {
-                border.RenderTransform = new ScaleTransform(1, 1);
-            }
+
         }
 
-
-        private void OpenPresentationGenerator(object sender, MouseButtonEventArgs e)
+        private void CharacterCreator_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Content = new PresentationGeneratorPage();
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
+
         }
 
-        private void OpenReportAssistant(object sender, MouseButtonEventArgs e)
+        private void MaintenanceAssistant_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Content = new ReportAssistantPage();
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
+
         }
 
-        private void OpenTestScenarioGenerator(object sender, MouseButtonEventArgs e)
+        private void InterfaceDesignAssistant_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Content = new TestScenarioGeneratorPage();
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
+
         }
 
-        private void OpenUIDesigner(object sender, MouseButtonEventArgs e)
+        private void EducationalMaterialCreator_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Content = new UIDesignerPage();
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
+
         }
 
-        private void OpenStoryCharacterGenerator(object sender, MouseButtonEventArgs e)
+        private void ImageGeneration_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Content = new StoryCharacterPage();
-        }
+            IQD_UI_Library.IQD_MessageBox.Show("رسالة", "ستتوفر قريبا....");
 
-        private void OpenFinancialAnalyzer(object sender, MouseButtonEventArgs e)
-        {
-            Content = new FinancialAnalyzerPage();
-        }
-
-        private void OpenEducationMaterialGenerator(object sender, MouseButtonEventArgs e)
-        {
-            Content = new EducationMaterialPage();
-        }
-
-        private void OpenMaintenanceAssistant(object sender, MouseButtonEventArgs e)
-        {
-            Content = new MaintenanceAssistantPage();
-        }
-
-        private void OpenImageGenerator(object sender, MouseButtonEventArgs e)
-        {
-            Content = new ImageGeneratorPage();
-        }
-
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
         }
     }
 }
