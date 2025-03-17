7806813b057df1ac15152516841ba146e59d1891
Yousif Ahmed <76800385+43ysf@users.noreply.github.com>
2025-03-17 09:08:02 +0300

    See this changes (#13)

---
+++ BLL/clsStage.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class clsStage : absBaseEntity
+    {
+        public string StageName { set; get; }
+        public clsStage(string stageName)
+        {
+            StageName = stageName;
+        }
+
+        private clsStage(int stageId, string stageName)
+        {
+            Id = stageId;
+            StageName += stageName;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            if (!Id.HasValue)
+                return await __AddAsync();
+            return await __UpdateAsync();
+        }
+
+        private async Task<bool> __AddAsync()
+        {
+            Id = await clsStageData.AddAsync(StageName);
+            return Id.HasValue;
+        }
+
+        public static async Task<clsStage?> GetByIdAsync(int stageId)
+        {
+            var data = await clsStageData.GetByIdAsync(stageId);
+            if (data == null) return null;
+            return FetchUserData(ref data);
+        }
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await clsTeacherData.GetAllAsDataTableAsync();
+
+        private async Task<bool> __UpdateAsync() => await clsSchoolData.UpdateAsync(Id, StageName);
+
+        public static async Task<bool> DeleteByIdAsync(int? schoolId) => await clsSchoolData.DeleteAsync(schoolId);
+
+        public async Task<bool> DeleteAsync() => await DeleteByIdAsync(Id);
+
+
+        internal static clsStage FetchUserData(ref Dictionary<string, object> dict) => new clsStage((int)dict["StageId"], (string)dict["StageName"] );
+
+
+
+    }
+}
