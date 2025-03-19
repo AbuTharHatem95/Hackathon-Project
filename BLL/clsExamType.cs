b25884a439132815d9fce8b3964d01d57586f36d
Abo_Sanad <167923142+AbuTharHatem95@users.noreply.github.com>
2025-03-20 00:50:11 +0300

    Add New Layer DAL and BLL

---
+++ BLL/clsExamType.cs
@@ -0,0 +1,58 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class clsExamType :absBaseEntity
+    {
+
+        public string TypeName { set; get; }
+        public clsExamType(string typeName)
+        {
+            this.TypeName = typeName;
+        }
+
+        private clsExamType(int typeId, string typeName)
+        {
+            Id = typeId;
+            TypeName = typeName;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            if (!Id.HasValue)
+                return await __AddAsync();
+            return await __UpdateAsync();
+        }
+
+        private async Task<bool> __AddAsync()
+        {
+            Id = await clsTeacherData.AddAsync(TypeName);
+            return Id.HasValue;
+        }
+
+        public static async Task<clsExamType?> GetByIdAsync(int typeId)
+        {
+            var data = await clsTeacherData.GetByIdAsync(typeId);
+            if (data == null) return null;
+            return FetchData(ref data);
+        }
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await clsExamTypeData.GetAllAsDataTableAsync();
+
+        private async Task<bool> __UpdateAsync() => await clsExamTypeData.UpdateAsync(Id, TypeName);
+
+        public static async Task<bool> DeleteByIdAsync(int? typeId) => await clsExamTypeData .DeleteAsync(typeId);
+
+        public async Task<bool> DeleteAsync() => await DeleteByIdAsync(Id);
+        internal static clsExamType FetchData(ref Dictionary<string, object> dict) => new clsExamType((int)dict["ExamTypeId"], (string)dict["TypeName"]);
+
+
+
+    }
+}
---
+++ DAL/clsQuestionsTypeData.cs
@@ -0,0 +1,73 @@
+﻿using DVLD_DataAccess;
+using Microsoft.Data.SqlClient;
+using MyLib_DotNet.DatabaseExecutor;
+using System.Data;
+
+namespace DAL
+{
+    public static class clsQuestionsTypeData
+    {
+        public static async Task<int?> AddAsync(string type)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Type", type),
+            };
+            return await CRUD.AddAsync("Sp_Add", parameters);
+        }
+        public static async Task<bool> UpdateAsync(int? questionTypeId, string type)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@QuestionTypeId", questionTypeId),
+                new SqlParameter("@Type", type),
+            };
+            return await CRUD.UpdateAsync("Sp_Update", parameters);
+        }
+
+        public static async Task<bool> DeleteAsync(int? questionTypeId)
+        {
+            if (questionTypeId == null) return false;
+            return await CRUD.DeleteAsync("SP_Delete", "QuestionTypeId", questionTypeId);
+        }
+
+        public static async Task<Dictionary<string, object>?> GetByIdAsync(int questionTypeId) => await CRUD.GetByColumnValueAsync("Sp_GetSettingsById", "QuestionTypeId", questionTypeId);
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await CRUD.GetAllAsDataTableAsync("Sp_GetAll");
+        public static async Task<DataTable?> GetAllQuestionTypesBySubjectAndClassAndStageAsync(int stClass, int stageId, int subjectId)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Class,", stClass),
+                new SqlParameter("@StageId", stageId),
+                new SqlParameter("@SubjectId", subjectId)
+
+            };
+            return await CRUD.GetAllAsDataTableAsync("sp_GetQuestionTypesForSpecificStageSubject", parameters);
+        }
+
+
+
+        public static DataTable GetAllQuestionTypesBySubjectAndClassAndStage(int subjectId)
+        {
+            string Query = @"SELECT DISTINCT 
+                     QuestionsType.Type AS QuestionType
+              FROM StageSubjects
+              JOIN SubjectQuestionTypesStages 
+                  ON StageSubjects.StageSubjectId = SubjectQuestionTypesStages.StageSubjectId
+              JOIN QuestionsType 
+                  ON SubjectQuestionTypesStages.QuestionTypeId = QuestionsType.QuestionTypeId
+              WHERE 
+                 StageSubjects.SubjectId = @SubjectId
+              ORDER BY QuestionsType.Type;";
+
+            SqlCommand cmd = new SqlCommand(Query);
+            cmd.Parameters.AddWithValue("@SubjectId", subjectId);
+
+            return CRUDs.GetAll(cmd);
+        }
+
+
+
+    }
+}
