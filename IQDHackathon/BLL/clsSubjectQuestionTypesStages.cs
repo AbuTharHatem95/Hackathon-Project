ebad78129b0c2df6e4a1b52b4d82432a62af9b51
Yousif <76800385+43ysf@users.noreply.github.com>
2025-03-20 20:30:42 +0300

    udate DllAndBll

---
+++ IQDHackathon/BLL/clsSubjectQuestionTypesStages.cs
@@ -0,0 +1,65 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class clsSubjectQuestionTypesStages : absBaseEntity
+    {
+        public clsStageSubject StageSubject { get; set; }
+        public clsQuestionsType QuestionsType { get; set; }
+
+        public clsSubjectQuestionTypesStages(clsStageSubject stageSubject, clsQuestionsType questionsType)
+        {
+            StageSubject = stageSubject;
+            QuestionsType = questionsType;
+        }
+        private clsSubjectQuestionTypesStages(int subjectQuestionTypeStageId, clsStageSubject stageSubject, clsQuestionsType questionsType)
+        {
+            Id = subjectQuestionTypeStageId;
+            StageSubject = stageSubject;
+            QuestionsType = questionsType;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            if (!Id.HasValue)
+                return await __AddAsync();
+            return await __UpdateAsync();
+        }
+
+        private async Task<bool> __AddAsync()
+        {
+            Id = await clsSubjectQuestionTypesStagesData.AddAsync(StageSubject.Id, QuestionsType.Id);
+            return Id.HasValue;
+        }
+
+        public static async Task<clsSubjectQuestionTypesStages?> GetByIdAsync(int subjectQuestionTypeStageId)
+        {
+            var data = await clsSubjectQuestionTypesStagesData.GetByIdAsync(subjectQuestionTypeStageId);
+            if (data == null) return null;
+            return FetchData(ref data);
+        }
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await clsSubjectQuestionTypesStagesData.GetAllAsDataTableAsync();
+
+        private async Task<bool> __UpdateAsync() => await clsSubjectQuestionTypesStagesData.UpdateAsync(Id, StageSubject.Id, QuestionsType.Id);
+
+        public static async Task<bool> DeleteByIdAsync(int? subjectQuestionTypeStageId) => await clsSubjectQuestionTypesStagesData.DeleteAsync(subjectQuestionTypeStageId);
+
+        public async Task<bool> DeleteAsync() => await DeleteByIdAsync(Id);
+
+        internal static clsSubjectQuestionTypesStages FetchData(ref Dictionary<string, object> dict) =>
+            new clsSubjectQuestionTypesStages((int)dict["QuestionTypeSubjectStageId"], clsStageSubject.FetchUserData(ref dict), clsQuestionsType.FetchUserData(ref dict));
+
+
+
+
+
+
+    }
+}
---
+++ IQDHackathon/DAL/clsExamsData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using MyLib_DotNet.DatabaseExecutor;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class clsExamsData
+    {
+
+        public static async Task<int?> AddAsync(int? stageSubjectId, int? teacherId, char branch, DateTime examDate, int? examTypeId, TimeSpan examTime)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@StageSubjectId", stageSubjectId),
+                new SqlParameter("@TeacherId", teacherId),
+                new SqlParameter("@Branch", branch),
+                new SqlParameter("@ExamDate", examDate),
+                new SqlParameter("@ExamTypeId", examTypeId),
+                new SqlParameter("@ExamTime", examTime),
+            };
+            return await CRUD.AddAsync("Sp_Add", parameters);
+        }
+        public static async Task<bool> UpdateAsync(int? examId, int? stageSubjectId, int? teacherId, char branch, DateTime examDate, int? examTypeId, TimeSpan examTime)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@ExamId", examId),
+                new SqlParameter("@StageSubjectId", stageSubjectId),
+                new SqlParameter("@TeacherId", teacherId),
+                new SqlParameter("@Branch", branch),
+                new SqlParameter("@ExamDate", examDate),
+                new SqlParameter("@ExamTypeId", examTypeId),
+                new SqlParameter("@ExamTime", examTime),
+            };
+            return await CRUD.UpdateAsync("Sp_Update", parameters);
+        }
+
+        public static async Task<bool> DeleteAsync(int? examId)
+        {
+            if (examId == null) return false;
+            return await CRUD.DeleteAsync("Sp_Delete", "ExamId", examId);
+        }
+
+        public static async Task<Dictionary<string, object>?> GetByIdAsync(int examId) => await CRUD.GetByColumnValueAsync("Sp_GetSettingsById", "ExamId", examId);
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await CRUD.GetAllAsDataTableAsync("Sp_GetAll");
+
+        //انشاء ستورد بوسيجر تبحث هل هناك بيانات في الجدول اذا نعم ترجع دشنري مليان اذا لا ترجع نلل
+        public static async Task<Dictionary<string, object>?> GetLastExam() => await CRUD.GetAsync("Sp_SelectTop 1 From Exams", null);
+
+
+        //ستور بروسيجر تحذف تيبلات التيجر و تحذف ايضا الاكزام تيبلز 
+        public static async Task<bool> DeleteAllTablesConnectsWithTheExamTable() => await CRUD.DeleteAsync("Sp_DeleteAllDataInTables", null);
+
+
+    }
+}
